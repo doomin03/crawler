5369815d10e6c4a98c703f3ba34c2b21f9fb65c4
doomin03 <111326974+doomin03@users.noreply.github.com>
2024-09-30 15:10:23 +0900

    배경색 추출

--- src/modules/CGV.py
+++ src/modules/CGV.py
@@ -1,4 +1,5 @@
 from modules.base import crawler_json
+# from base import crawler_json
 
 import json
 
@@ -13,7 +14,10 @@ class main_view(crawler_json):
     def __init__(self) -> None:
         super().__init__()
         self.data = {
-            "side_image_url" : None,
+            "banner" : {
+                "side_image_url" : None,
+                "color" : None
+            },
             "main" : {
                 "title" : None,
                 "video_url" : None,
@@ -38,6 +42,11 @@ class main_view(crawler_json):
             EC.visibility_of_element_located((By.CSS_SELECTOR, "body > div > a:nth-child(1) > img"))
         ).get_attribute("src")
 
+        body = self.wait.until(
+            EC.visibility_of_element_located((By.CSS_SELECTOR, '.cgv_ad_top'))
+        )
+        body_bg_color = body.value_of_css_property('background-color')
+
         self.driver.switch_to.default_content()
 
         main_div = self.wait.until(
@@ -50,8 +59,8 @@ class main_view(crawler_json):
 
         content = main_div.find_element(By.ID, "ctl00_PlaceHolderContent_AD_DESCRIPTION_NM").text
         
-
-        self.data["side_image_url"] = side_url
+        self.data["banner"]["side_image_url"] = side_url
+        self.data["banner"]["color"] = body_bg_color
         self.data["main"]["title"] = title
         self.data["main"]["video_url"] = video_url
         self.data["main"]["content"] = content
